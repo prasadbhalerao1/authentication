a2825d46acf4a2697ca18936ea73e08959adfbac
Prasad Bhalerao <prasad9a38@gmail.com>
2025-12-24 22:05:05 +0530

    Setup routes boilerplate code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config({
   path: "./.env",
@@ -9,6 +10,9 @@ dotenv.config({
 await connectDB();
 
 const app = express();
+
+//using routes
+app.use("/api/v1", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
